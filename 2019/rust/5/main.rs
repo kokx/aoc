226b4aa6bee40e26e8d9a54b485c4f43cb6b8371
Pieter Kokx <pieter@kokx.nl>
2019-12-05 17:28:37 +0100

    Improved readability and added input vector

--- 2019/rust/5/main.rs
+++ 2019/rust/5/main.rs
@@ -1,5 +1,6 @@
 use std::io::{self, BufRead};
 use std::convert::TryInto;
+use std::collections::VecDeque;
 
 // guess: 47780111 (too high)
 
@@ -11,99 +12,122 @@ fn main() {
     }
 }
 
-fn get_param_value(program : &Vec<i32>, mode : i32, loc : usize) -> Option<i32> {
-    let mode = mode % 10;
-    if mode == 1 {
-        return Some(program[loc]);
+fn mode(modes : i32, pos : usize) -> i32 {
+    let mut mode = modes;
+
+    for _ in 0..pos {
+        mode /= 10;
     }
 
-    let pos : usize = program[loc].try_into().unwrap();
-    if pos > program.len() {
-        return None;
+    mode % 10
+}
+
+fn get_param(program : &Vec<i32>, mode : i32, loc : usize) -> Option<i32> {
+    match mode {
+        1 => Some(program[loc]),
+        0 => {
+            let pos : usize = program[loc].try_into().unwrap();
+            if pos > program.len() {
+                return None;
+            }
+
+            Some(program[pos])
+        },
+        _ => None
     }
-    return Some(program[pos]);
 }
 
 /// Execute an intcode program
 ///
 /// # Arguments
 ///
-/// * `program` - Vector of positive integers
+/// * `program` - Vector of integers
+/// * `input` - Vector of input values, read from front to back
 ///
 /// # Remarks
 ///
 /// Executes an intcode program as specified at:
 /// - https://adventofcode.com/2019/day/5
 ///
-fn exec(mut program : Vec<i32>) -> Option<i32> {
+fn exec(mut program : Vec<i32>, input : Vec<i32>) -> Option<i32> {
     let mut ic = 0;
+
+    let mut input : VecDeque<i32> = input.try_into().unwrap();
 
     loop {
         // split the op into op and mode else
         let modes = program[ic] / 100;
         let op = program[ic] % 100;
 
-        // end of the program, do not read parameters
-        if op == 99 {
-            //println!("{}", program[ic]);
-            break;
-        } else if op == 1 || op == 2 || op == 7 || op == 8 {
-            //println!("{} => ({} {}) {} {} {}", program[ic], modes, op, program[ic+1], program[ic+2], program[ic+3]);
-            // read 3 parameters
-            let fir = get_param_value(&program, modes / 1, ic+1)?;
-            let sec = get_param_value(&program, modes / 10, ic+2)?;
+        match op {
+            // end of the program
+            99 => break,
+            3 | 4 => {
+                // input and output, with one parameter
 
-            let out : usize = program[ic+3].try_into().unwrap();
+                match op {
+                    3 => {
+                        let a : usize = program[ic+1].try_into().unwrap();
+                        program[a] = input.pop_front()?;
+                    },
+                    4 => {
+                        let a = get_param(&program, mode(modes, 0), ic+1)?;
+                        println!("{}", a);
+                    },
+                    _ => ()
+                }
 
-            if out >= program.len() {
+                if op == 3 {
+                } else if op == 4 {
+                    // read 1 parameter and output its value
+
+                }
+                ic = ic + 2;
+            },
+            5 | 6 => {
+                // conditional jumps, with two parameters
+                let a = get_param(&program, mode(modes, 0), ic+1)?;
+                let b = get_param(&program, mode(modes, 1), ic+2)?;
+
+                // 5: jump if not zero
+                // 6: jump if zero
+                if (a == 0 && op == 6) || (a != 0 && op == 5) {
+                    ic = b.try_into().unwrap();
+                } else {
+                    ic += 3;
+                }
+            },
+            1 | 2 | 7 | 8 => {
+                // with three parameters
+                let a = get_param(&program, mode(modes, 0), ic+1)?;
+                let b = get_param(&program, mode(modes, 1), ic+2)?;
+
+                let out : usize = program[ic+3].try_into().unwrap();
+
+                if out >= program.len() {
+                    return None;
+                }
+
+                program[out] = match op {
+                    // +
+                    1 => a + b,
+                    // *
+                    2 => a * b,
+                    // <
+                    7 => (a < b).try_into().unwrap(),
+                    // ==
+                    8 => (a == b).try_into().unwrap(),
+                    // all otehr cases, just return c
+                    _ => program[out]
+                };
+
+
+                ic = ic + 4;
+            },
+            _ => {
+                println!("Unkown instruction: {}", op);
                 return None;
             }
-
-            program[out] = match op {
-                // +
-                1 => fir + sec,
-                // *
-                2 => fir * sec,
-                // <
-                7 => (fir < sec).try_into().unwrap(),
-                // ==
-                8 => (fir == sec).try_into().unwrap(),
-                // do not change anything with a wrong opcode
-                _ => program[out],
-            };
-
-            //println!("-> {}", program[out]);
-
-            ic = ic + 4;
-        } else if op == 3 || op == 4 {
-            println!("{} => {} {}", program[ic], op, program[ic+1]);
-            if op == 3 {
-                let fir : usize = program[ic+1].try_into().unwrap();
-                // TODO: write actual input
-                program[fir] = 5;
-                println!("Writing to {}", fir);
-            } else if op == 4 {
-                // read 1 parameter and output its value
-                let fir = get_param_value(&program, modes / 1, ic+1)?;
-                println!("out: {}", fir);
-                println!("---------------------");
-
-            }
-            ic = ic + 2;
-        } else if op == 5 || op == 6 {
-            let fir = get_param_value(&program, modes / 1, ic+1)?;
-            let sec = get_param_value(&program, modes / 10, ic+2)?;
-
-            // 5: jump if not zero
-            // 6: jump if zero
-
-            if (fir == 0 && op == 6) || (fir != 0 && op == 5) {
-                ic = sec.try_into().unwrap();
-            } else {
-                ic += 3;
-            }
-        } else {
-            println!("Unknown instruction: {}", op);
         }
     }
 
@@ -115,8 +139,17 @@ fn run(line : String) {
                                   .map(|s| s.parse().unwrap())
                                   .collect();
 
-    match exec(program.clone()) {
-        Some(result) => println!("Part one: {}", result),
-        _ => println!("Part one failed")
+    println!("Part one:");
+    println!("---------");
+    if exec(program.clone(), vec![1]) == None {
+        println!("Part one failed!");
+    }
+
+    println!();
+
+    println!("Part two:");
+    println!("---------");
+    if exec(program.clone(), vec![5]) == None {
+        println!("Part two failed!");
     }
 }
