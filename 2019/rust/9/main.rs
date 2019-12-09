fcbd1630b1f2fe0b994c49e19301a652e555121a
Pieter Kokx <pieter@kokx.nl>
2019-12-09 09:37:53 +0100

    Day 9

---
+++ 2019/rust/9/main.rs
@@ -0,0 +1,218 @@
+use std::io::{self, BufRead};
+use std::convert::TryInto;
+use std::collections::VecDeque;
+
+// guess: 47780111 (too high)
+
+fn main() {
+    let stdin = io::stdin();
+
+    for line in stdin.lock().lines() {
+        run(line.unwrap());
+    }
+}
+
+fn mode(modes : i64, pos : usize) -> i64 {
+    let mut mode = modes;
+
+    for _ in 0..pos {
+        mode /= 10;
+    }
+
+    mode % 10
+}
+
+enum IntcodeResult {
+    Finished(Vec<i64>),
+    WaitInput(Vec<i64>)
+}
+
+#[derive(Debug)]
+struct IntcodeComputer {
+    program : Vec<i64>,
+    input : VecDeque<i64>,
+    ic : usize,
+    relative_base : i64
+}
+
+impl IntcodeComputer {
+
+    fn new(prog : Vec<i64>) -> IntcodeComputer {
+        IntcodeComputer {
+            program: prog,
+            input: VecDeque::new(),
+            ic: 0,
+            relative_base: 0
+        }
+    }
+
+    fn push_input(&mut self, value : i64) {
+        self.input.push_back(value);
+    }
+
+    fn get_param(&self, mode : i64, loc : usize) -> Option<i64> {
+        match mode {
+            1 => Some(self.program[loc]),
+            2 => {
+                let pos = self.program[loc];
+                let pos = self.relative_base + pos;
+
+                let pos : usize = pos.try_into().unwrap();
+                if pos > self.program.len() {
+                    return None;
+                }
+
+                Some(self.program[pos])
+            },
+            0 => {
+                let pos : usize = self.program[loc].try_into().unwrap();
+                if pos > self.program.len() {
+                    return None;
+                }
+
+                Some(self.program[pos])
+            },
+            _ => None
+        }
+    }
+
+    fn exec(&mut self) -> Option<IntcodeResult> {
+        let mut output : Vec<i64> = Vec::new();
+
+        loop {
+            // split the op into op and mode else
+            let modes = self.program[self.ic] / 100;
+            let op = self.program[self.ic] % 100;
+
+            //println!("OP: {}, modes: {}", op, modes);
+
+            match op {
+                // end of the program
+                99 => return Some(IntcodeResult::Finished(output)),
+                3 | 4 | 9 => {
+                    // relative base adjustment
+                    // input and output, with one parameter
+
+                    match op {
+                        3 => {
+                            let a : usize = match mode(modes, 0) {
+                                2 => {
+                                    let val = self.program[self.ic+1];
+                                    let fin = self.relative_base + val;
+                                    fin.try_into().unwrap()
+                                },
+                                _ => self.program[self.ic+1].try_into().unwrap()
+                            };
+                            if let Some(value) = self.input.pop_front() {
+                                self.program[a] = value;
+                            } else {
+                                return Some(IntcodeResult::WaitInput(output));
+                            }
+                        },
+                        4 => {
+                            let a = self.get_param(mode(modes, 0), self.ic+1)?;
+                            //println!("out: {}", a);
+                            output.push(a);
+                        },
+                        9 => {
+                            self.relative_base = self.relative_base + self.get_param(mode(modes, 0), self.ic+1)?;
+                            //println!("base: {}", self.relative_base);
+                        }
+                        _ => ()
+                    }
+
+                    self.ic = self.ic + 2;
+                },
+                5 | 6 => {
+                    // conditional jumps, with two parameters
+                    let a = self.get_param(mode(modes, 0), self.ic+1)?;
+                    let b = self.get_param(mode(modes, 1), self.ic+2)?;
+
+                    // 5: jump if not zero
+                    // 6: jump if zero
+                    if (a == 0 && op == 6) || (a != 0 && op == 5) {
+                        self.ic = b.try_into().unwrap();
+                    } else {
+                        self.ic += 3;
+                    }
+                },
+                1 | 2 | 7 | 8 => {
+                    // with three parameters
+                    let a = self.get_param(mode(modes, 0), self.ic+1)?;
+                    let b = self.get_param(mode(modes, 1), self.ic+2)?;
+
+                    let out : usize = match mode(modes, 2) {
+                        2 => {
+                            let val = self.program[self.ic+3];
+                            let fin = self.relative_base + val;
+                            fin.try_into().unwrap()
+                        }
+                        _ => self.program[self.ic+3].try_into().unwrap()
+                    };
+
+                    if out >= self.program.len() {
+                        return None;
+                    }
+
+                    self.program[out] = match op {
+                        // +
+                        1 => a + b,
+                        // *
+                        2 => a * b,
+                        // <
+                        7 => (a < b).try_into().unwrap(),
+                        // ==
+                        8 => (a == b).try_into().unwrap(),
+                        // all otehr cases, just return c
+                        _ => self.program[out]
+                    };
+
+
+                    self.ic = self.ic + 4;
+                },
+                _ => {
+                    println!("Unkown instruction: {}", op);
+                    return None;
+                }
+            }
+        }
+    }
+}
+
+fn run(line : String) {
+    let program: Vec<i64> = line.split(",")
+                                  .map(|s| s.parse().unwrap())
+                                  .collect();
+
+    // increase program size
+    let mut program = program;
+    for _ in 0..10000 {
+        program.push(0);
+    }
+
+    println!("Part one:");
+    println!("---------");
+
+    let mut computer = IntcodeComputer::new(program.clone());
+
+    computer.push_input(1);
+
+    match computer.exec() {
+        Some(IntcodeResult::Finished(output)) => println!("{:?}", output),
+        Some(IntcodeResult::WaitInput(output)) => println!("Wait: {:?}", output),
+        None => println!("Failed or something")
+    };
+
+    println!("Part two:");
+    println!("---------");
+
+    let mut computer = IntcodeComputer::new(program.clone());
+
+    computer.push_input(2);
+
+    match computer.exec() {
+        Some(IntcodeResult::Finished(output)) => println!("{:?}", output),
+        Some(IntcodeResult::WaitInput(output)) => println!("Wait: {:?}", output),
+        None => println!("Failed or something")
+    };
+}
